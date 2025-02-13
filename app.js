1edbc46fd50eeb68c68598cf1e853ec4618120e4
Shawn Nguru <nguru.shawn@student.greenriver.edu>
2025-02-13 17:15:47 -0600

    app and home modified

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.post('/thankyou', (req, res) => {
+    console.log(req.body);
+    res.render('confirm', { order } );
+});
+
 app.listen(PORT, () => {
     console.log(`Server running http://localhost:${PORT}`);
 })
