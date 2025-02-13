aec14ed2889772a0127745ba3751eac89c73a507
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-02-13 15:14:51 -0800

    test

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.post('/thankyou', (req, res) => {
+    const order = {
+        
+    }
+
+    console.log(req.body);
+    res.render('confirm', { order });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running http://localhost:${PORT}`);
 })
